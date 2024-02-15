a713897a4b48f6997e621ec525f5f7f4bdf128a2
Vaz1k28 <edik22082006@gmail.com>
2024-02-15 22:37:53 +0200

    Task 2 done

--- src/fizz_buzz.py
+++ src/fizz_buzz.py
@@ -1,3 +1,10 @@
 def fizz_buzz(param):
   #Type your code here
-  return
+  if param % 3 == 0 and param % 5 == 0:
+    return "FizzBuzz"
+  elif param % 5 == 0:
+    return "Buzz"
+  elif param % 3 == 0:
+    return "Fizz"
+  else:
+    return str(param)
