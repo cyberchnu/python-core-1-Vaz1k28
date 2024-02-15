cebb40127b4eab9a6eb2622a41ace1370ea63780
Vaz1k28 <edik22082006@gmail.com>
2024-02-15 21:31:53 +0200

    Task 4 done

--- src/calculate_fuel.py
+++ src/calculate_fuel.py
@@ -1,6 +1,6 @@
 def calculate_fuel(distance):
   # Type your code
-  if distance < 0:
-    return distance * 10
-  elif distance > 0:
-    return 100
+  if distance <= 0:
+  return distance * 10
+  else
+  return 100
